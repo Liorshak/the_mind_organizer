ea529a65819fca3a80a281ed725665f5b59c6611
lior shaked <Liorshak1986@gmail.com>
2021-10-21 15:19:27 +0300

    finished 1st part

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
-// part 1
-
 // class Bubble (id, text, done, hidden, type, priority)
-
+let Bubble = class {
+  constructor(id, text, done, hidden, type, priority) {
+    this.id = id;
+    this.text = text;
+    this.done = done;
+    this.hidden = hidden;
+    this.type = type;
+    this.priority = priority;
+  }
+};
 // general stuff
-
 // collect elements
 // let ... document.get..
-
-
 // part 2
-// add respective listners to initial elements 
-    // createBubble
-
+// add respective listners to initial elements
+// createBubble
 // part 3 - functions createBubble() -> creates a draggable div bubble
-
-    // sanity validations (text not empty for instance)-> checkBubble
-
-    // div > input with text / radio button ->
-        // delete (hidden=true) / done check box / priority input
+// sanity validations (text not empty for instance)-> checkBubble
+// div > input with text / radio button ->
+// delete (hidden=true) / done check box / priority input
