3cf14090d82fc25f0dd17ded9cf6ce76eabb4425
lior shaked <Liorshak1986@gmail.com>
2021-10-21 13:15:46 +0300

    finished index

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// part 1
+
+// class Bubble (id, text, done, hidden, type, priority)
+
+// general stuff
+
+// collect elements
+// let ... document.get..
+
+
+// part 2
+// add respective listners to initial elements 
+    // createBubble
+
+// part 3 - functions createBubble() -> creates a draggable div bubble
+
+    // sanity validations (text not empty for instance)-> checkBubble
+
+    // div > input with text / radio button ->
+        // delete (hidden=true) / done check box / priority input
